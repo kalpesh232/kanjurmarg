d8af7441382dd9a54b50ea54c2dcbc65432b51bd
kalpes232 <kalpesh4111993@gmail.com>
2025-12-20 11:15:31 +0530

    add String slicing code

--- jobs/ynd.py
+++ jobs/ynd.py
@@ -1,17 +1,33 @@
 # ---------- Singleton design pattern in python ---------- #
-class Config:
-    __instance = None
+# class Config:
+#     __instance = None
 
-    def __new__(cls):
-        if not cls.__instance:
-            cls.__instance = super().__new__(cls)
-            cls.__instance = {"Them" : "Dark", "Version" : 1.0}
-        return cls.__instance
+#     def __new__(cls):
+#         if not cls.__instance:
+#             cls.__instance = super().__new__(cls)
+#             cls.__instance = {"Them" : "Dark", "Version" : 1.0}
+#         return cls.__instance
     
-config1 = Config()
-config2 = Config()
+# config1 = Config()
+# config2 = Config()
 
-print(config1 is config2)
-# print(config1.setting)
+# print(config1 is config2)
+# # print(config1.setting)
 
+# From a string, extract all unique substrings starting from length 3 and return them in length-wise order.
+a = "xyzyxzxzyyzx"
 
+result = []
+seen = set()
+
+n = len(a)
+
+for length in range(3, n + 1):          # length 3 → n
+    for i in range(n - length + 1):
+        sub = a[i:i + length]
+        if sub not in seen:
+            seen.add(sub)
+            result.append(sub)
+
+print(tuple(result))
+# ('xyz', 'yzx', 'xyzx')
